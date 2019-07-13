35046527239b764d58e3e0c4a2ce36465bc0c889
Raúl Montón <raul_m_p@me.com>
2019-07-14 00:11:53 +0200

    Added animation on BoletinActivity.

--- app/src/main/java/com/raulmonton/cerbuapp/BoletinActivity.java
+++ app/src/main/java/com/raulmonton/cerbuapp/BoletinActivity.java
@@ -3,9 +3,13 @@ package com.raulmonton.cerbuapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class BoletinActivity extends AppCompatActivity {
+
+    AnimationDrawable boletinAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,10 @@ public class BoletinActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ImageView boletinTop = findViewById(R.id.boletinTop);
+        boletinTop.setImageResource(R.drawable.boletin_animation);
+        boletinAnimation = (AnimationDrawable) boletinTop.getDrawable();
+        boletinAnimation.start();
     }
 }
