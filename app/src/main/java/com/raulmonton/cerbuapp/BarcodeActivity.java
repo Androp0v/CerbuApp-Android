eceb1f119c41eec060b851e132250f4f9d811594
Raúl Montón <raul_m_p@me.com>
2019-09-22 13:25:53 +0200

    Tweaks to barcode UI.

--- app/src/main/java/com/raulmonton/cerbuapp/BarcodeActivity.java
+++ app/src/main/java/com/raulmonton/cerbuapp/BarcodeActivity.java
@@ -6,17 +6,24 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class BarcodeActivity extends AppCompatActivity {
@@ -24,6 +31,41 @@ public class BarcodeActivity extends AppCompatActivity {
     private static final int PICK_FROM_GALLERY = 3;
     Uri imageUri;
     ImageView barcode;
+
+    public static void saveFile(Context context, Bitmap b, String picName){
+        FileOutputStream fos;
+        try {
+            fos = context.openFileOutput(picName, Context.MODE_PRIVATE);
+            b.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //fos.close();
+        }
+    }
+
+    public Bitmap loadBitmap(Context context, String picName){
+        Bitmap b = null;
+        FileInputStream fis;
+        try {
+            fis = context.openFileInput(picName);
+            b = BitmapFactory.decodeStream(fis);
+        }
+        catch (FileNotFoundException e) {
+            b = BitmapFactory.decodeResource(getResources(),R.drawable.ic_addbarcode);
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //fis.close();
+        }
+        return b;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +75,14 @@ public class BarcodeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //Set brightness to max
+        WindowManager.LayoutParams layout = getWindow().getAttributes();
+        layout.screenBrightness = 1F;
+        getWindow().setAttributes(layout);
+
+        //Try to load barcode and set OnClickListener to import/change it
         barcode = findViewById(R.id.barcodeImageView);
+        barcode.setImageBitmap(loadBitmap(getApplicationContext(),"barcodeSaved"));
         barcode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,9 +119,11 @@ public class BarcodeActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode,resultCode,data);
-        if (requestCode == PICK_FROM_GALLERY) {
+        if (requestCode == PICK_FROM_GALLERY && null != data) {
             imageUri = data.getData();
             barcode.setImageURI(imageUri);
+            Bitmap bitmap = ((BitmapDrawable)barcode.getDrawable()).getBitmap();
+            saveFile(getApplicationContext(),bitmap,"barcodeSaved");
         }
     }
 }
