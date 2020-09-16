9d224af46db26b58dc26ef861a0f68e210eb3865
Raúl Montón Pinillos <raul_m_p@me.com>
2020-09-17 01:35:53 +0200

    Implemented QR scanning [UNTESTED].

--- app/src/main/java/com/raulmonton/cerbuapp/QRScanSheet.java
+++ app/src/main/java/com/raulmonton/cerbuapp/QRScanSheet.java
@@ -1,32 +1,105 @@
 package com.raulmonton.cerbuapp;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import java.io.IOException;
 
 public class QRScanSheet extends Dialog {
 
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private Context localContext;
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
+
     public QRScanSheet(@NonNull Context context) {
         super(context);
+        localContext = context;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SurfaceView qrView = findViewById(R.id.surfaceView);
+        final SurfaceView qrView = findViewById(R.id.surfaceView);
+
+        barcodeDetector = new BarcodeDetector.Builder(localContext)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+
+        cameraSource = new CameraSource.Builder(localContext, barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080)
+                .setAutoFocusEnabled(true) //you should add this feature
+                .build();
+
+        // Setup barcodeDetector processor to process detected QR codes
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+                Toast.makeText(localContext.getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if (barcodes.size() != 0) {
+                    dismiss();
+                }
+            }
+        });
+
+        // Setup SurfaceView qrView to display camera frames
+
+        qrView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(localContext, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        cameraSource.start(qrView.getHolder());
+                    } else {
+                        ActivityCompat.requestPermissions(null, new
+                                String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+
+        });
     }
 
     @Override
